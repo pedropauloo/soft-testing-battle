40c7407d181c6fd4d875df643b21e5cce87ed67e
Pedro Paulo <42193455+pedropauloo@users.noreply.github.com>
2024-12-20 00:08:35 -0300

    feat: add test de atributos invalidos dos personagens

--- src/test/java/batalha/PersonagemTest.java
+++ src/test/java/batalha/PersonagemTest.java
@@ -10,16 +10,16 @@ class PersonagemTest {
 
     @ParameterizedTest
     @CsvSource({
-            "2, 5, 5, 5",    // Ataque < 3
-            "5, 2, 5, 5",    // Defesa < 3
-            "5, 5, 2, 5",    // Velocidade < 3
-            "5, 5, 5, 2",    // Resistencia < 3
-            "5, 3, 6, 5",    // Ataque < Velocidade
-            "6, 3, 5, 5",    // Velocidade < Ataque
-            "5, 6, 5, 5",    // Defesa > Ataque
-            "5, 5, 6, 5",    // Defesa > Velocidade
-            "5, 5, 5, 6",    // Resistencia > Ataque
-            "5, 5, 5, 6"     // Resistencia > Velocidade
+            "2, 6, 6, 6",    // Ataque < 3
+            "6, 2, 6, 6",    // Defesa < 3
+            "6, 6, 2, 6",    // Velocidade < 3
+            "6, 6, 6, 2",    // Resistencia < 3
+            "5, 4, 6, 5",    // Ataque < Velocidade
+            "6, 4, 5, 5",    // Velocidade < Ataque
+            "5, 6, 4, 5",    // Defesa > Ataque
+            "5, 4, 6, 5",    // Defesa > Velocidade
+            "5, 4, 5, 6",    // Resistencia > Ataque
+            "5, 5, 4, 6"     // Resistencia > Velocidade
     })
     void testAssassinoInvalido(int ataque, int defesa, int velocidade, int resistencia) {
         assertThrows(IllegalArgumentException.class, () -> {
@@ -54,16 +54,16 @@ class PersonagemTest {
 
     @ParameterizedTest
     @CsvSource({
-            "2, 5, 5, 5",    // Ataque < 3
-            "5, 2, 5, 5",    // Defesa < 3
-            "5, 5, 2, 5",    // Velocidade < 3
-            "5, 5, 5, 2",    // Resistencia < 3
+            "2, 6, 6, 6",    // Ataque < 3
+            "6, 2, 6, 6",    // Defesa < 3
+            "6, 6, 2, 6",    // Velocidade < 3
+            "6, 6, 6, 2",    // Resistencia < 3
             "7, 4, 4, 5",    // Resistencia < Ataque
             "5, 4, 4, 7",    // Ataque < Resistencia
             "5, 7, 3, 5",    // Defesa > Resistencia
             "5, 6, 4, 5",    // Defesa > Ataque
             "5, 3, 7, 5",    // Velocidade > Resistencia
-            "5, 6, 4, 5",    // Velocidade > Ataque
+            "5, 4, 6, 5",    // Velocidade > Ataque
 
     })
     void testGuerreiroInvalido(int ataque, int defesa, int velocidade, int resistencia) {
@@ -99,8 +99,8 @@ class PersonagemTest {
 
     @ParameterizedTest
     @CsvSource({
-            "5, 5, 5, 4",    // Soma < 20
-            "8, 6, 6, 5",    // Soma > 20
+            "5, 5, 4, 5",    // Soma < 20
+            "8, 6, 6, 8",    // Soma > 20
     })
     void testSomaAtributosPersonagemInvalidas(int ataque, int defesa, int velocidade, int resistencia) {
         assertThrows(IllegalStateException.class, () -> {
@@ -108,7 +108,17 @@ class PersonagemTest {
                     .comAtaque(ataque)
                     .comDefesa(defesa)
                     .comVelocidade(velocidade)
-                    .comResistencia(resistencia);
-        });
+                    .comResistencia(resistencia)
+                    .doTipoGuerreiro();
+        }, "Esperava excecao para os atributos fornecidos.");
+
+        assertThrows(IllegalStateException.class, () -> {
+            PersonagemBuilder.construaUmPersonagem()
+                    .comAtaque(ataque)
+                    .comDefesa(defesa)
+                    .comVelocidade(velocidade)
+                    .comResistencia(resistencia)
+                    .doTipoAssassino();
+        }, "Esperava excecao para os atributos fornecidos.");
     }
 }
